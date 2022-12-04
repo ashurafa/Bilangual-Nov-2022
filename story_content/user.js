3a23500915eb0e58faf132f9c8a9d6c6e2265254
unknown <ashurafa@hotmail.com>
2022-12-04 16:32:21 +0300

    With Google Sheets

--- story_content/user.js
+++ story_content/user.js
@@ -2,26 +2,38 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "60Ai5pWop0E":
+      case "5b4t6LfMDwJ":
         Script1();
         break;
-      case "5t1jLHRxwZD":
+      case "5uFIg2UB9wi":
         Script2();
         break;
-      case "5lNGi0WeCPb":
+      case "6QtmSy0wjQu":
         Script3();
         break;
-      case "6kQ53V4dltR":
+      case "6NdeOHfZtZ1":
         Script4();
         break;
-      case "5cmsEXlBRjc":
+      case "6IzhdOXJa8G":
         Script5();
         break;
-      case "6gWg5CCPuo7":
+      case "6MVNx8fO1eK":
         Script6();
         break;
-      case "5XGfGckmHb0":
+      case "6PHIIYhC69o":
         Script7();
+        break;
+      case "61ghCJZ4K1N":
+        Script8();
+        break;
+      case "6gkQRblhAm4":
+        Script9();
+        break;
+      case "5o1XuSY4adw":
+        Script10();
+        break;
+      case "6TPisVrb6vV":
+        Script11();
         break;
   }
 }
@@ -56,17 +68,61 @@ gsap.to( QBox , { duration:1.0, scale: 1.1, ease: "elastic.out(1.5, 0.15)" });
 
 function Script4()
 {
+  // Tranfer the SL word to the Google Sheet (https://docs.google.com/spreadsheets/d/1UXND96QQ9Gey5qSGB05MzezJhjXw6tgNwF0YG1RCgG4/edit#gid=0)
+
+ 
+
+ // Replace this URL with your Web App URL
+const url = "https://script.google.com/macros/s/AKfycbwGFf464IxSkI6Yo2j6mGuHmRwOF1he3ipp6DzfDheUCPslAnm5YIIhLa1MahWlTLN5wQ/exec";
+const player = GetPlayer();
+var Word = player.GetVar("WordA"); // Replace "UserFeedback" with your Storyline Variable.
+var Collected = Word + " ⓪";
+
+fetch(url,{
+  method: 'POST',
+  mode: 'no-cors',
+  cache: 'no-cache',
+  headers: {'Content-Type': 'application/json'},
+  redirect: 'follow',
+  body: JSON.stringify({text: Collected})
+});
+}
+
+function Script5()
+{
+  // Tranfer the SL word to the Google Sheet (https://docs.google.com/spreadsheets/d/1UXND96QQ9Gey5qSGB05MzezJhjXw6tgNwF0YG1RCgG4/edit#gid=0)
+
+ 
+
+// Replace this URL with your Web App URL
+const url = "https://script.google.com/macros/s/AKfycbwGFf464IxSkI6Yo2j6mGuHmRwOF1he3ipp6DzfDheUCPslAnm5YIIhLa1MahWlTLN5wQ/exec";
+const player = GetPlayer();
+var Word = player.GetVar("WordA"); // Replace "UserFeedback" with your Storyline Variable.
+var Collected = Word + " ①";
+
+fetch(url,{
+  method: 'POST',
+  mode: 'no-cors',
+  cache: 'no-cache',
+  headers: {'Content-Type': 'application/json'},
+  redirect: 'follow',
+  body: JSON.stringify({text: Collected})
+});
+}
+
+function Script6()
+{
   Listen();
 English();
 
 }
 
-function Script5()
+function Script7()
 {
   Say();
 }
 
-function Script6()
+function Script8()
 {
   // getting variables from SL for the dimenssions scaleX and scaleY. Both scaleX and scaleY are SL numeric variables.
 
@@ -82,7 +138,51 @@ gsap.to( QBox , { duration:1.0, scale: 1.1, ease: "elastic.out(1.5, 0.15)" });
 
 }
 
-function Script7()
+function Script9()
+{
+  // Tranfer the SL word to the Google Sheet (https://docs.google.com/spreadsheets/d/1UXND96QQ9Gey5qSGB05MzezJhjXw6tgNwF0YG1RCgG4/edit#gid=0)
+
+ 
+
+ // Replace this URL with your Web App URL
+const url = "https://script.google.com/macros/s/AKfycbwGFf464IxSkI6Yo2j6mGuHmRwOF1he3ipp6DzfDheUCPslAnm5YIIhLa1MahWlTLN5wQ/exec";
+const player = GetPlayer();
+var Word = player.GetVar("WordA"); // Replace "UserFeedback" with your Storyline Variable.
+var Collected = Word + " ⓪";
+
+fetch(url,{
+  method: 'POST',
+  mode: 'no-cors',
+  cache: 'no-cache',
+  headers: {'Content-Type': 'application/json'},
+  redirect: 'follow',
+  body: JSON.stringify({text: Collected})
+});
+}
+
+function Script10()
+{
+  // Tranfer the SL word to the Google Sheet (https://docs.google.com/spreadsheets/d/1UXND96QQ9Gey5qSGB05MzezJhjXw6tgNwF0YG1RCgG4/edit#gid=0)
+
+ 
+
+// Replace this URL with your Web App URL
+const url = "https://script.google.com/macros/s/AKfycbwGFf464IxSkI6Yo2j6mGuHmRwOF1he3ipp6DzfDheUCPslAnm5YIIhLa1MahWlTLN5wQ/exec";
+const player = GetPlayer();
+var Word = player.GetVar("WordA"); // Replace "UserFeedback" with your Storyline Variable.
+var Collected = Word + " ①";
+
+fetch(url,{
+  method: 'POST',
+  mode: 'no-cors',
+  cache: 'no-cache',
+  headers: {'Content-Type': 'application/json'},
+  redirect: 'follow',
+  body: JSON.stringify({text: Collected})
+});
+}
+
+function Script11()
 {
   // iSL1 to load eJS1 my_functions.js during project start (a simlar iSL2 loads eJS2)
 // SL Trigger to call External JS file called my_functions.js
